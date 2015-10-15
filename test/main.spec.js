a926c35685aba426091b62df9f0790c521136cf3
Cyrus Rhett Walker II <rhettwalker86@gmail.com>
2015-10-15 12:21:57 -0400

    Implemented  Module for Testing

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,13 +2,28 @@ var assert = chai.assert;
 var expect = chai.expect;
 
 describe("The Address Book App", function(){
+
   describe("the contact service", function(){
-    it("should have a property contacts, an array",function(){
+
+    beforeEach(function(){
       module('AddressBook');
       inject(function($injector){
         contactService = $injector.get("contactService");
+        //module for intercepting http requests
+        $httpBackend = $injector.get("$httpBackend");
       });
-      expect(contactService.contacts).to.be.an('array');
     })
+
+    it("should have an array of property contacts", function(){
+      expect(contactService.contacts).to.be.an('array');
+    });
+
+    it("should call the backend", function(){
+      $httpBackend.expectGET("http://localhost:9001/contacts")
+        //chain response
+        .respond(200,[]);
+      //runs all requests, catch any unknown http requests
+      $httpBackend.flush();
+    });
   })
 })
